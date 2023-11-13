40e24a86bdeefca32da9896393f868a57e943fbe
Tim Hay <burninghayproductions@gmail.com>
2023-11-13 14:58:29 -0500

    When the nav links are hovered over, and clicked I have included functionality

--- static/script.js
+++ static/script.js
@@ -1,10 +1,29 @@
 const mainHeading = document.querySelector('#main-content h1');
 const navLinks = document.querySelectorAll('#sidebar a');
+const navElements = document.querySelectorAll('#nav-elements a');
 
-function updateH1Text(event) {
+function updateH1Text(event) { //For Changing the Heading in the Main content area
     mainHeading.textContent = this.textContent;
 }
 
-navLinks.forEach(link => {
+function makeActive(event) { //For creating the background-color behind nav elements
+    navElements.forEach(link => {
+        link.classList.remove('active');
+    });
+    this.classList.add('active');
+    event.stopPropagation();
+}
+
+navLinks.forEach(link => { //updateH1Text(event): For Changing the Heading in the Main content area
     link.addEventListener('click', updateH1Text);
+});
+
+navElements.forEach(link => { //makeActive(event): For creating the background-color behind nav elements
+    link.addEventListener('click', makeActive)
+});
+
+document.body.addEventListener('click', function() {
+    navElements.forEach(link => {
+        link.classList.remove('active');
+    });
 });
